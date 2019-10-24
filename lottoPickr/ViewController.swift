6ed95f7d4d3f02b6945d05672aade45bd01ee18a
jporras1 <javi.jr2150@gmail.com>
2019-10-23 22:35:46 -0500

    Initial Commit

--- lottoPickr/ViewController.swift
+++ lottoPickr/ViewController.swift
@@ -9,12 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //MARK: Properties
+    
+    //MARK: Init
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .gray
     }
 
+    //MARK: Handlers
 
 }
 
